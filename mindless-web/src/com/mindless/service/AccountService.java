3d408edf1919c05f5f24b019de39085667f8f1f3
mcchicken <jeff.gibson@mindless-software.com>
2012-06-09 00:07:13 -0400

    Refactored Account object to a base class.  Added Notification base class.  Updated import in account service.  Added account notification API.

--- mindless-web/src/com/mindless/service/AccountService.java
+++ mindless-web/src/com/mindless/service/AccountService.java
@@ -1,10 +1,14 @@
 package com.mindless.service;
 
-import com.mindless.bean.Account;
+import com.mindless.bean.base.Account;
+import com.mindless.bean.base.Notification;
 import com.mindless.exception.MindlessRuntimeException;
 
 public interface AccountService {
 	public Account getAccountByGuid(String guid) throws MindlessRuntimeException;
+	public void notifyAccount(Notification notification, Account account) throws MindlessRuntimeException;
+	
+	//Administration API
 	public void creditPointsToAccount(long numberOfPoints) throws MindlessRuntimeException;
 	public void banAccount(String guid) throws MindlessRuntimeException;
 	public void upgradeAccount(long newAccountLevel) throws MindlessRuntimeException;
