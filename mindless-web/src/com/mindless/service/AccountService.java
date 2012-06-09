189dbd2e9b6b116990712501b3756b8f0ba48539
mcchicken <jeff.gibson@mindless-software.com>
2012-06-09 00:34:26 -0400

    Added Avatar base class and API to create an avatar for an account.

--- mindless-web/src/com/mindless/service/AccountService.java
+++ mindless-web/src/com/mindless/service/AccountService.java
@@ -1,15 +1,19 @@
 package com.mindless.service;
 
 import com.mindless.bean.base.Account;
+import com.mindless.bean.base.Avatar;
 import com.mindless.bean.base.Notification;
 import com.mindless.exception.MindlessRuntimeException;
 
 public interface AccountService {
 	public Account getAccountByGuid(String guid) throws MindlessRuntimeException;
+	public Account createAccount(Account account) throws MindlessRuntimeException;
 	public void notifyAccount(Notification notification, Account account) throws MindlessRuntimeException;
+	public void createAvatarForAccount(Avatar avatar, Account account) throws MindlessRuntimeException;
 	
 	//Administration API
 	public void creditPointsToAccount(long numberOfPoints) throws MindlessRuntimeException;
 	public void banAccount(String guid) throws MindlessRuntimeException;
+	public void lockAccountForTimePeriod(long milliseconds) throws MindlessRuntimeException;
 	public void upgradeAccount(long newAccountLevel) throws MindlessRuntimeException;
 }
