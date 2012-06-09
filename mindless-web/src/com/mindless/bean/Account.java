5ee4d62c33dd88ea7046d40d2fcbe2622078b340
mcchicken <jeff.gibson@mindless-software.com>
2012-06-08 22:31:13 -0400

    Added a few Account administration and retrieval services.  Bean for Account and runtime exception stubbed for compilation.

---
+++ mindless-web/src/com/mindless/bean/Account.java
@@ -0,0 +1,13 @@
+package com.mindless.bean;
+
+/**
+ * Bean to represent a user's account.  An account can belong 
+ * to an end user or Mindless Software associate for 
+ * administration purposes.  
+ * 
+ * @author mcchicken
+ *
+ */
+public class Account {
+
+}
--- mindless-web/src/com/mindless/service/AccountService.java
+++ mindless-web/src/com/mindless/service/AccountService.java
@@ -1,5 +1,11 @@
 package com.mindless.service;
 
-public interface AccountService {
+import com.mindless.bean.Account;
+import com.mindless.exception.MindlessRuntimeException;
 
+public interface AccountService {
+	public Account getAccountByGuid(String guid) throws MindlessRuntimeException;
+	public void creditPointsToAccount(long numberOfPoints) throws MindlessRuntimeException;
+	public void banAccount(String guid) throws MindlessRuntimeException;
+	public void upgradeAccount(long newAccountLevel) throws MindlessRuntimeException;
 }
