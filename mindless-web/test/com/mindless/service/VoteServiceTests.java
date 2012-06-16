e0d74792688e1fd031aac5280ae63c47ccf9c3a3
mcchicken <jeff.gibson@mindless-software.com>
2012-06-16 00:22:23 -0400

    Added negative vote count increment.

--- mindless-web/test/com/mindless/service/VoteServiceTests.java
+++ mindless-web/test/com/mindless/service/VoteServiceTests.java
@@ -2,18 +2,37 @@ package com.mindless.service;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.mindless.bean.Poll;
 
 public class VoteServiceTests {
 	private VoteService testVoteService = new VoteService();
+	private Poll expectedPoll;
+	
+	@Before
+	public void intializeExpectedPoll(){
+		this.expectedPoll = new Poll();
+	}
+	
+	@After
+	public void destroyExpectedPoll(){
+		this.expectedPoll = null;
+	}
 	
 	@Test
 	public void votePositiveForPollUpdatesPositiveVoteCount(){
-		Poll expectedPoll = new Poll();
 		expectedPoll.setPositiveVotes(0);
 		Poll actualPoll = testVoteService.addPositiveVote(expectedPoll);
-		assertEquals("Vote total is one", 1, actualPoll.getPositiveVotes());
+		assertEquals("Vote total is not one", 1, actualPoll.getPositiveVotes());
+	}
+	
+	@Test
+	public void voteNegativeForPollUpdatesNegativeVoteCount(){
+		expectedPoll.setNegativeVotes(0);
+		Poll actualPoll = testVoteService.addNegativeVote(expectedPoll);
+		assertEquals("Vote total is not one", 1, actualPoll.getNegativeVotes());
 	}
 }
