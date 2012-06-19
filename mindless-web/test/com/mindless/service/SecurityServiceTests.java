5188f497d892f1400994d0eb1671acf794f5b90b
mcchicken <jeff.gibson@mindless-software.com>
2012-06-18 21:20:12 -0400

    Remove unused imports.

--- mindless-web/test/com/mindless/service/SecurityServiceTests.java
+++ mindless-web/test/com/mindless/service/SecurityServiceTests.java
@@ -1,8 +1,6 @@
 package com.mindless.service;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 import java.util.Map;
